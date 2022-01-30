e825d55ea0f4b60486d09c49a2981ac94f8c9ec2
Yuyakichi <3.16.yuyakichi@gmail.com>
2022-01-30 23:30:25 +0900

    make header

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// $(function() {
-//     $('.slide').each(function() {
-//       // スライド（画像）の数を取得
-//       var $slides = $(this).find('img'),
-//           slideNum = $slides.length,
-//           currentIdx = 0; // 何番目か
-
-//       // 最初の画像をフェードイン
-//       $(".slide img").eq(currentIdx).fadeIn();
-
-//       // 3秒後に次のスライドを表示
-//       setTimeout(dispNextSlide, 3000);
-
-//       // 次のスライドを表示するメソッド
-//       function dispNextSlide() {
-//         var nextIdx = currentIdx + 1;
-
-//         // 最後のスライドの場合ははじめに戻る
-//         if (nextIdx > (slideNum - 1)) {
-//           nextIdx = 0
-//         }
-
-//         // 現在のスライドをフェードアウト
-//         $(".slide img").eq(currentIdx).fadeOut();
-
-//         // 次のスライドをフェードイン
-//         $(".slide img").eq(nextIdx).fadeIn();
-
-//         // インデックスを更新
-//         currentIdx = nextIdx;
-//       }
-//     });
-//    });
-
 $(function () {
   $('.single-item').slick({
     accessibility: true,
@@ -41,3 +7,17 @@ $(function () {
     fade: true,
   });
 });
+
+$(function () {
+  $(window).scroll(function () {
+    $('.left-to-right').each(function () {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 150) {
+        $(this).addClass('scrollin');
+      }
+    });
+  });
+});
+
